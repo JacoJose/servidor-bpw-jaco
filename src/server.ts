f4572e535ee0ee74c4af038dc38e3c97e3e4b7bc
Jacó José <jaco.jose.tecnico@gmail.com>
2026-06-17 19:33:43 -0300

    adicionando servidor no github

---
+++ src/server.ts
@@ -0,0 +1,37 @@
+import express, { type Request, type Response } from 'express'
+import { User } from './models/user'
+
+const app = express();
+const PORT = 3000;
+
+app.use(express.json());
+
+app.get('/', (request: Request, response: Response) => {
+  const user = new User('Jacó','jaco@gmail.com','123') 
+  
+  console.log(user)
+
+  response.json({
+    message: 'OI!',
+    timestamp: new Date(),
+    user: user,
+    status: 'API funcionando'
+  })
+});
+
+app.get('/', (request: Request, response: Response) => {
+  const user = new User('Jacó','jaco@gmail.com','123') 
+  
+  console.log(user.verificarSenha('1234'))
+
+  response.json({
+    message: 'OI!',
+    timestamp: new Date(),
+    user: user,
+    status: 'API funcionando'
+  })
+});
+
+app.listen(PORT, () => {
+  console.log(`Servidor rodando em http://localhost:${PORT}`);
+});
